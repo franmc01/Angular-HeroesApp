f0e813a780be197596ba1c7b846ddda566bfdc2b
francisco-1998 <francmarinc@gmail.com>
2021-02-03 18:52:46 -0500

    Definiendo rutas principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NotFoundErrorComponent } from './shared/not-found-error/not-found-error.component';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotFoundErrorComponent,
     ErrorPageComponent
   ],
   imports: [
