363857880ccda615fbe48a391a12cd1c5c1dd62e
francisco-1998 <francmarinc@gmail.com>
2021-02-05 11:56:14 -0500

    Correccion de un error infantil que estaba haciendo

--- src/app/material-design/material-design.module.ts
+++ src/app/material-design/material-design.module.ts
@@ -6,17 +6,21 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 
 
 @NgModule({
   exports: [
+    MatProgressSpinnerModule,
+    MatGridListModule,
     MatSidenavModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
   ]
 })
 export class MaterialDesignModule { }
